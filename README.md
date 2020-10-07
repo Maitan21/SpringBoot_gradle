93dba78e7a3b64af9ef27257cc751ccea5f08eb0
Maitan21 <oklfg2002@gmail.com>
2020-10-08 00:46:44 +0900

    ADD RequstBody(API)

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 ## 브랜치 정보
-- v.1.0.1 : MVC and Template Tu torial
+- v.1.0.1 : MVC and Template Tutorial
+- v.1.0.2 : What is API : RequestBody (JSON)
--- src/main/java/practice/SpringPractice/controller/HelloController.java
+++ src/main/java/practice/SpringPractice/controller/HelloController.java
@@ -3,8 +3,8 @@ package practice.SpringPractice.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -23,5 +23,38 @@ public class HelloController {
     public String helloMvc(@RequestParam("name") String name, Model model) {
         model.addAttribute("name",name);
         return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody //http에서 body부에 데이터를 직접 넣어주겠다는 뜻 즉, 데이터를 그대로 내려줌
+    public String helloString(@RequestParam("name") String name){
+        return "hello " + name;
+    }
+
+    //중요!
+    //JSON 방식으로 데이터 아웃풋
+    @GetMapping("hello-api")
+    @ResponseBody // JSON으로 반환이 기본 셋팅
+    public Hello helloApi(@RequestParam("name") String name){
+        //Hello 객체 새 객체 생성 위와 다르게 객체를 넘겨줌
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    // 객체 메소드
+    /*java bean 기본양식 */
+    static class Hello {
+        private  String name;
+
+        //꺼내올때
+        public String getName() {
+            return name;
+        }
+
+        //세팅할떄
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
