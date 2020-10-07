436f189783a15e2f70712b383bc4dff2331fb719
Maitan21 <oklfg2002@gmail.com>
2020-10-08 00:21:58 +0900

    MVC and Template Tutorial

--- src/main/java/practice/SpringPractice/controller/HelloController.java
+++ src/main/java/practice/SpringPractice/controller/HelloController.java
@@ -3,6 +3,8 @@ package practice.SpringPractice.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -15,5 +17,11 @@ public class HelloController {
         //return 실행파일위치
         return "hello";
         //(attributeValue)"hello!!"가 hello.html 에서 {data}부분에 치환됨
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name",name);
+        return "hello-template";
     }
 }
