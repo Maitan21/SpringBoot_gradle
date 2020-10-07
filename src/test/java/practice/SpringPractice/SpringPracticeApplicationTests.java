f7ad6594cdaafa4cdd1473c315b3409f9f0a82f5
Maitan21 <oklfg2002@gmail.com>
2020-10-07 23:37:18 +0900

    Create SpringBoot Project

---
+++ src/test/java/practice/SpringPractice/SpringPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package practice.SpringPractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
